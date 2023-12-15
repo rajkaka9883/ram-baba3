c0e66cad2dbd7745c31e488f43f865387d16031c
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-12-15 20:52:46 +0530

    Update myplane.py

--- plugins/myplane.py
+++ plugins/myplane.py
@@ -2,25 +2,16 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command('myplan'))
-async def myplan(_, message):
+async def buy_premium(_, message):
     text = """<b>ʜᴀʏ Ram.., 👋
 
 ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴀᴄᴛɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴘʀᴇᴍɪᴜᴍ ᴛʜᴇɴ
 ᴄʟɪᴄᴋ ᴏɴ /upgrade ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴘʟᴀɴ</b>"""
-    keyboard = InlineKeyboardMarkup([[  
-        InlineKeyboardButton("🫰 Buy Premium 💸", url="https://t.me/leotgadmin_bot")],  
-        [InlineKeyboardButton("Cancel Premium", callback_data="close_data")]])
-    await update.message.edit(text=text, reply_markup=keyboard)
-    
-
-@Client.on_message(filters.command('myplan'))
-async def myplan(_, message):
-    text = """<b>ʜᴀʏ Ram.., 👋
-
-ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴀᴄᴛɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴘʀᴇᴍɪᴜᴍ ᴛʜᴇɴ
-ᴄʟɪᴄᴋ ᴏɴ /upgrade ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴘʟᴀɴ</b>"""
-    keyboard = InlineKeyboardMarkup([[  
-        InlineKeyboardButton("🫰 Buy Premium 💸", url="https://t.me/leotgadmin_bot")],  
-        [InlineKeyboardButton("Cancel Premium", callback_data="close_data")]])
-    await update.message.edit(text=text, reply_markup=keyboard)
    
+
+@Client.on_message(filters.private & filters.command(["myplan"]))
+async def buy_premium(_, message):
+    text = """<b>ʜᴀʏ Ram.., 👋
+
+ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴀᴄᴛɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴘʀᴇᴍɪᴜᴍ ᴛʜᴇɴ
+ᴄʟɪᴄᴋ ᴏɴ /upgrade ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴘʟᴀɴ</b>"""
