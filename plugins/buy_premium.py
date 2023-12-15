613cb845e4ab49daa91a2b4cfc08ebf21e5080f5
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-12-15 22:27:57 +0530

    Update buy_premium.py

--- plugins/buy_premium.py
+++ plugins/buy_premium.py
@@ -3,17 +3,19 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command('upgrade'))
 async def upgrade(_, message):
-    text = """ᴄᴏᴍɪɴɢ ꜱᴏᴏɴ 😀"""
+    text = f"""<b>ʜᴀʏ {message.from_user.mention}..,👋
+   ᴏᴜʀ ᴛʜɪꜱ ꜰʀᴇᴀᴛᴜʀᴇ ᴄᴏᴍɪɴɢ ꜱᴏᴏɴ . ᴀꜱ ꜱᴏᴏɴ ᴀꜱ ᴘᴏꜱꜱɪʙʟᴇ😀</b>"""
     keyboard = InlineKeyboardMarkup([[  
-        InlineKeyboardButton("🫰 Buy Premium 💸", url="https://t.me/leotgadmin_bot")],  
-        [InlineKeyboardButton("Cancel Premium", callback_data="close_data")]])
+        InlineKeyboardButton("♻️ ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ ꜱᴜʙꜱᴄʀɪᴘᴛɪᴏɴ  / ꜱᴇɴᴅ ꜱᴄʀᴇᴇɴꜱʜᴏᴛ ♻️", url="https://t.me/PERSONAL_CHAT_ASSISTANT_BOT")],  
+        [InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ  ❌", callback_data="close_data")]])
     await update.message.edit(text=text, reply_markup=keyboard)
 
 
 @Client.on_message(filters.command('upgrade'))
 async def upgrade(_, message):
-    text = """ᴄᴏᴍɪɴɢ ꜱᴏᴏɴ 😀"""
+    text = f"""<b>ʜᴀʏ {message.from_user.mention}..,👋
+   ᴏᴜʀ ᴛʜɪꜱ ꜰʀᴇᴀᴛᴜʀᴇ ᴄᴏᴍɪɴɢ ꜱᴏᴏɴ . ᴀꜱ ꜱᴏᴏɴ ᴀꜱ ᴘᴏꜱꜱɪʙʟᴇ😀</b>"""
     keyboard = InlineKeyboardMarkup([[  
-        InlineKeyboardButton("🫰 Buy Premium 💸", url="https://t.me/leotgadmin_bot")],  
-        [InlineKeyboardButton("Cancel Premium", callback_data="close_data")]])
+        InlineKeyboardButton("♻️ ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ ꜱᴜʙꜱᴄʀɪᴘᴛɪᴏɴ / ꜱᴇɴᴅ ꜱᴄʀᴇᴇɴꜱʜᴏᴛ ♻️", url="https://t.me/PERSONAL_CHAT_ASSISTANT_BOT")],  
+        [InlineKeyboardButton("❌ ᴄʟᴏꜱᴇ  ❌", callback_data="close_data")]])
     await message.reply_text(text=text, reply_markup=keyboard)
