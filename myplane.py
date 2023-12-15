7027ce9608bf0c0cfe7f88d3a857c8fb5b42bb20
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-12-15 21:00:01 +0530

    Update myplane.py

--- myplane.py
+++ myplane.py
@@ -2,17 +2,17 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command('myplan'))
-async def buy_premium(_, message):
-    text = """<b>ʜᴀʏ Ram.., 👋
+async def myplane(_, message):
+    text = f"""<b>ʜᴀʏ {message.from_user.mention}.., 👋
 
 ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴀᴄᴛɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴘʀᴇᴍɪᴜᴍ ᴛʜᴇɴ
 ᴄʟɪᴄᴋ ᴏɴ /upgrade ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴘʟᴀɴ</b>"""
    
 
 @Client.on_message(filters.private & filters.command(["myplan"]))
-async def buy_premium(_, message):
-    text = """<b>ʜᴀʏ Ram.., 👋
+async def myplane(_, message):
+    text = f"""<b>ʜᴀʏ {message.from_user.mention}.., 👋
 
 ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴀᴄᴛɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴘʀᴇᴍɪᴜᴍ ᴛʜᴇɴ
 ᴄʟɪᴄᴋ ᴏɴ /upgrade ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴘʟᴀɴ</b>"""
-    
+
