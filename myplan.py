d3bfece8ffb1e6f94fab5653daa0b8ea659a88a5
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-12-15 20:42:36 +0530

    Update myplan.py

--- myplan.py
+++ myplan.py
@@ -1,1 +1,26 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command('myplan'))
+async def myplan(_, message):
+    text = """<b>ʜᴀʏ Ram.., 👋
+
+ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴀᴄᴛɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴘʀᴇᴍɪᴜᴍ ᴛʜᴇɴ
+ᴄʟɪᴄᴋ ᴏɴ /upgrade ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴘʟᴀɴ</b>"""
+    keyboard = InlineKeyboardMarkup([[  
+        InlineKeyboardButton("🫰 Buy Premium 💸", url="https://t.me/leotgadmin_bot")],  
+        [InlineKeyboardButton("Cancel Premium", callback_data="close_data")]])
+    await update.message.edit(text=text, reply_markup=keyboard)
+    
+
+@Client.on_message(filters.command('myplan'))
+async def myplan(_, message):
+    text = """<b>ʜᴀʏ Ram.., 👋
+
+ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴀᴄᴛɪᴠᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴꜱ, ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴘʀᴇᴍɪᴜᴍ ᴛʜᴇɴ
+ᴄʟɪᴄᴋ ᴏɴ /upgrade ᴛᴏ ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ᴘʟᴀɴ</b>"""
+    keyboard = InlineKeyboardMarkup([[  
+        InlineKeyboardButton("🫰 Buy Premium 💸", url="https://t.me/leotgadmin_bot")],  
+        [InlineKeyboardButton("Cancel Premium", callback_data="close_data")]])
+    await update.message.edit(text=text, reply_markup=keyboard)
+   
